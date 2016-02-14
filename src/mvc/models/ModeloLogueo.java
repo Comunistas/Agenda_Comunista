a3f2691260f8599fc6b76860c42b892b89a71779
Vladislav Zedano <Zedano@DSK_ZEDANO>
2016-02-14 11:04:40 -0500

    Creado el modelo de Logueo

--- src/mvc/models/ModeloLogueo.java
+++ src/mvc/models/ModeloLogueo.java
@@ -1,5 +1,7 @@
 package mvc.models;
 
+import javax.servlet.http.Cookie;
+
 import dao.beans.CamaradaDTO;
 import services.MantenimientoService;
 
@@ -7,9 +9,10 @@ public class ModeloLogueo {
 
 	MantenimientoService sMantenimiento = new MantenimientoService();
 	
-	CamaradaDTO cam = null;
+	private CamaradaDTO cam = null;
+	private Cookie cookieLogueado = null, cookiePwd = null, cookieRecordar = null;
 	
-	public boolean pwdValido(String codigo, String password){
+	public boolean pwdValido(String codigo, String password, boolean recordar){
 		boolean ok = false;
 		
 		try{
@@ -17,8 +20,6 @@ public class ModeloLogueo {
 			
 			if(cam.getPwd_cam().equals(password)){
 				ok = true;
-				
-				
 			}
 			
 		}catch(Exception e){
@@ -29,4 +30,58 @@ public class ModeloLogueo {
 		return ok;
 	}
 
+	
+	
+	public void crearCookieLogueado(){
+		
+		cookieLogueado = new Cookie("logueado", cam.getCod_cam());
+		
+		
+		cookieLogueado.setMaxAge(60*20);
+		
+		
+		cookieLogueado.setPath("/");
+		
+	}
+	
+	public void crearCookiePwd(){
+		
+		cookiePwd = new Cookie("recordarPwd", cam.getPwd_cam());
+
+		cookiePwd.setMaxAge(60*60*24*365);
+		
+		cookiePwd.setPath("/");
+	}
+	
+	public void crearCookieRecordar(){
+		
+		cookieRecordar = new Cookie("recordarUsuario", cam.getCod_cam());
+
+		cookieRecordar.setMaxAge(60*60*24*365);
+		
+		cookieRecordar.setPath("/");
+	}
+	
+	
+	
+	public Cookie getCookieLogueado(){
+		
+		crearCookieLogueado();
+		return cookieLogueado;
+		
+	}
+	
+	public Cookie getCookiePwd(){
+		
+		crearCookiePwd();
+		return cookiePwd;
+		
+	}
+	
+	public Cookie getCookieRecordar(){
+		
+		crearCookieRecordar();
+		return cookieRecordar;
+		
+	}
 }
